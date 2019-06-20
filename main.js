0fdb34dc4e152b4b3a9d936a9cf07dabb1a20362
Patrick Bassett <bassettptty@gmail.com>
2019-06-20 09:26:30 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 const app = document.getElementById('root');
+const dropDown = document.getElementById('dropDown');
+let dropDownItem;
 
-const logo = document.createElement('img');
-logo.src = 'logo.png';
+
 
 const container = document.createElement('div');
 container.setAttribute('class', 'container');
 
-app.appendChild(logo);
+
 app.appendChild(container);
 
 
 const searchButton = document.getElementById('searchButton');
-
+const searchArea = document.getElementById('search');
 
 
 let movies = [];
@@ -29,7 +30,9 @@ request.onload = function () {
     data.forEach(movie => {
       
       movies[counter] = movie;
-    counter++;
+    
+    
+     counter++;
    
     });
   } else {
@@ -37,12 +40,37 @@ request.onload = function () {
     errorMessage.textContent = `Gah, it's not working!`;
     app.appendChild(errorMessage);
   }
+  loadDropDown();
 }
 
 
 
 request.send();
 
+
+function loadDropDown(){
+
+  for(let i =0; i< movies.length;i++){
+  dropDownItem =document.createElement('option');
+ 
+  dropDownItem.textContent = movies[i].title;
+  dropDown.append(dropDownItem);
+
+ 
+  
+  dropDown.onclick = function(){
+    console.log('y');
+
+    
+   searchArea.value = dropDown.value;
+  }
+
+  
+  }
+
+
+
+}
 
 
 
@@ -57,6 +85,7 @@ searchButton.onclick = function(){
       break;
     }else{
       console.log('uh oh');
+      
     }
   }
 
@@ -67,17 +96,38 @@ function load(movie){
 
   const card = document.createElement('div');
   card.setAttribute('class', 'card');
+  card.style.backgroundColor = '#bcc0c6';
+  card.style.padding = '5px';
+  card.style.border = '2px solid black';
+
+  
+  const hr = document.createElement('hr');
 
   const h1 = document.createElement('h1');
   h1.textContent = movie.title;
 
+  const span = document.createElement('span');
+  span.style.display = 'inline';
+
+  const h4 = document.createElement('h4');
+  h4.textContent = movie.producer
+
+  const releaseDate = document.createElement('h4');
+  releaseDate.textContent = movie.release_date;
 
   const p = document.createElement('p');
   movie.description = movie.description;
   p.textContent = `${movie.description}...`;
 
+
+
   container.appendChild(card);
+  
       card.appendChild(h1);
+      card.appendChild(hr);
+      card.appendChild(span);
+      span.append(h4);
+      span.append(releaseDate);
       card.appendChild(p);
       
 
